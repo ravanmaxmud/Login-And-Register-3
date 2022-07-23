933125c10fa39da5ed8a8a73e5487aa8fae2706c
ravanmaxmud <ravanim@code.edu.az>
2022-07-23 23:41:03 +0400

    Program.Cs Has Been Changed

--- LoginAndRegister3/LoginAndRegister3/UI/Program.cs
+++ LoginAndRegister3/LoginAndRegister3/UI/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Login_and_Register.Aplication_Locig;
+using System;
 
-namespace LoginAndRegister3
+namespace Login_and_Register
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           
+            while (true)
+            {
+                Console.WriteLine("======================================================================");
+                Console.WriteLine("Welcome to our Program");
+
+                Console.WriteLine("======================================================================");
+                Console.WriteLine("Thees are Our Command");
+                Console.WriteLine("/register");
+                Console.WriteLine("/login");
+                Console.WriteLine("/exit");
+
+                Console.Write("Please Enter Command :");
+                string command = Console.ReadLine();
+                if (command == "/register")
+                {
+                    Authentication.Register();
+                }
+                else if (command == "/login")
+                {
+                    Authentication.Login();
+                }
+                else if (command == "/exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Command :");
+                }
+            }
         }
     }
 }
+

9711c41a66a1f73193db53ba3088e5158a7c856d
ravanmaxmud <ravanim@code.edu.az>
2022-08-10 11:56:53 +0400

    Inbox Class Aded Models Folder

--- LoginAndRegister3/LoginAndRegister3/DataBase/Models/Inbox.cs
+++ LoginAndRegister3/LoginAndRegister3/DataBase/Models/Inbox.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LoginAndRegister3.DataBase
+namespace LoginAndRegister3.DataBase.Models
 {
-    internal class Inbox
+     class Inbox
     {
     }
 }

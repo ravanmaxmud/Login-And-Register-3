7a2221567135a7fb90ca1cb622d8457b56a10d56
ravanmaxmud <ravanim@code.edu.az>
2022-08-10 12:42:42 +0400

    Inbox Class

--- LoginAndRegister3/LoginAndRegister3/DataBase/Models/Inbox.cs
+++ LoginAndRegister3/LoginAndRegister3/DataBase/Models/Inbox.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Login_and_Register.DataBase.Models;
+using LoginAndRegister3.DataBase.Models.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace LoginAndRegister3.DataBase.Models
 {
-     class Inbox
+    class Inbox : Entitiy<int>
     {
+        public string Natification { get; set; }
+        public User UseR { get; set; }
+        public Inbox(string natification, User user)
+        {
+            Natification = natification;
+            UseR = user; 
+        }
     }
 }
